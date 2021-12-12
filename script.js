b4b5c4a88f960bf61e150e07743cc9e0471e373b
Matyanson <matyas.perem@seznam.cz>
2021-12-12 22:03:42 +0100

    chore: gallery page

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ toggleBtn.addEventListener('click', () => navElement.classList.toggle('opened'))
 
 //media controls
 const media = document.getElementById('bg_video');
-const muteIcon = document.getElementById('mute').children[0];
-const replayIcon = document.getElementById('replay').children[0];
+const muteIcon = document.getElementById('mute')?.children[0];
+const replayIcon = document.getElementById('replay')?.children[0];
 
-replayIcon.addEventListener('click', () => media.currentTime = 0);
+replayIcon?.addEventListener('click', () => media.currentTime = 0);
 
-muteIcon.addEventListener('click', () => {
+muteIcon?.addEventListener('click', () => {
     if(media.muted) {
         media.muted = false;
         muteIcon.classList = 'fi fi-volume-down'
@@ -21,3 +21,42 @@ muteIcon.addEventListener('click', () => {
         muteIcon.classList = 'fi fi-volume-mute';
     }
 });
+
+//gallery
+const pictures = document.querySelectorAll('.gallery picture');
+
+for(p of pictures) {
+    p.addEventListener('click', toggleFullscreen);
+}
+function toggleFullscreen(e) {
+    const elem = e.target;
+    if(isFullscreen())
+        closeFullscreen();
+    else
+        openFullscreen(elem);
+}
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+function isFullscreen() {
+    return document.isFullScreen ||
+    document.fullscreenElement ||
+    document.webkitFullscreenElement ||
+    document.mozFullScreenElement ||
+    document.msFullscreenElement;
+}
